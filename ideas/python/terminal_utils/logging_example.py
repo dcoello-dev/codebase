f6cfac738f471044a65060640d87856804d6d93d
dcoello-dev <dcpulido91@gmail.com>
2024-08-30 12:58:18 +0200

    Update python logging
    Signed-off-by: dcoello-dev <dcpulido91@gmail.com>

--- ideas/python/terminal_utils/logging_example.py
+++ ideas/python/terminal_utils/logging_example.py
@@ -4,10 +4,21 @@
 # sandbox_env: python
 
 import logging
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description="example script on how to use argparse")
+
+parser.add_argument('--verbose', '-v', action='count', default=1)
+
+args = parser.parse_args()
+
+args.verbose = 70 - (10*args.verbose) if args.verbose > 0 else 0
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
-    level=logging.INFO)
+    level=args.verbose)
 
 if __name__ == "__main__":
     logging.info("hello world!")
