3309b1f371bc095f5ac79e254e23e9a17cd387bc
dcoello-dev <dcpulido91@gmail.com>
2024-10-22 14:23:21 +0200

    Update python logging idea
    Signed-off-by: dcoello-dev <dcpulido91@gmail.com>

--- ideas/python/terminal_utils/logging_example.py
+++ ideas/python/terminal_utils/logging_example.py
@@ -12,13 +12,19 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--verbose', '-v', action='count', default=1)
 
+parser.add_argument(
+    '-l', '--log_level',
+    default="info",
+    help="app log level")
+
 args = parser.parse_args()
 
 args.verbose = 70 - (10*args.verbose) if args.verbose > 0 else 0
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
-    level=args.verbose)
+    level=eval(f"logging.{args.log_level.upper()}"))
 
 if __name__ == "__main__":
-    logging.info("hello world!")
+    print(args)
+    logging.info(args)
